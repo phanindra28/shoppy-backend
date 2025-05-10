11b9d1b8157bd0358f2653b092eabb97e5566881
Phanindra <tphanindra28@gmail.com>
2025-05-10 13:56:39 +0530

    Add User table and prisma setup

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { UsersService } from './users.service';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post()
+  @UseInterceptors(NoFilesInterceptor())
+  createUser(@Body() request: CreateUserRequest) {
+    console.log(request);
+    return this.usersService.createUser(request);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(data: CreateUserRequest) {
+    try {
+      console.log(data);
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists.');
+      }
+      throw error;
+    }
+  }
+}
