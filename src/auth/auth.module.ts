601a37a2c52d90bec2b1d0d3b216fdbcaa360626
Phanindra <tphanindra28@gmail.com>
2025-05-12 01:04:43 +0530

    Add JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: configService.getOrThrow('JWT_EXPIRATION') },
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ConfigModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { compare } from 'bcrypt';
+import { User } from '@prisma/client';
+import ms from 'ms';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayloadInterface } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
   async verifyUser(email: string, password: string) {
     try {
       const user = await this.userService.getUser({ email });
@@ -16,5 +26,29 @@ export class AuthService {
     } catch (error) {
       throw new UnauthorizedException('Invalid credentials');
     }
+  }
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getMilliseconds() +
+        ms(
+          this.configService.getOrThrow<string>(
+            'JWT_EXPIRATION',
+          ) as unknown as ms.StringValue,
+        ),
+    );
+    const tokenPayload: TokenPayloadInterface = {
+      userId: user.id,
+    };
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      secure: true,
+      httpOnly: true,
+      expires,
+    });
+
+    return { tokenPayload };
   }
 }
